c86027f8cc4a024f2fdbc2db134c0c9c26e9f6ef
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-03-10 12:19:01 +0000

    includes

--- gemmi_tools/out/build/x64-Debug/include/gemmi/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/cwild/Documents/GitHub/gemmi_tools/gemmi_tools/include/gemmi
-# Build directory: C:/Users/cwild/Documents/GitHub/gemmi_tools/gemmi_tools/out/build/x64-Debug/include/gemmi
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(cpptest "C:/Users/cwild/Documents/GitHub/gemmi_tools/gemmi_tools/out/build/x64-Debug/include/gemmi/cpptest.exe")
-set_tests_properties(cpptest PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/cwild/Documents/GitHub/gemmi_tools/gemmi_tools/include/gemmi/CMakeLists.txt;303;add_test;C:/Users/cwild/Documents/GitHub/gemmi_tools/gemmi_tools/include/gemmi/CMakeLists.txt;0;")
